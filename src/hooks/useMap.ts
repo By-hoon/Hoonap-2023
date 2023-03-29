71a8ed4c5ea06f0bcfa2f8b1fe251aaaa630086f
Yeunhun Bae <sbaru1997@naver.com>
2023-03-29 18:11:47 +0900

    feat: 현재위치 지정 기능 구현

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,18 +1,38 @@
 import { useEffect, useRef, useState } from "react";
 
 function useMap() {
+  const [myLocation, setMyLocation] = useState<{ latitude: number; longitude: number } | string>("");
+
   const mapRef = useRef<HTMLElement | null | any>(null);
 
   useEffect(() => {
-    let currentPosition = [37.4862618, 127.1222903];
-
-    mapRef.current = new naver.maps.Map("map", {
-      center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
-      zoomControl: true,
-    });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      window.alert("현재 위치를 알 수 없습니다. 기본 위치로 지정합니다.");
+      setMyLocation({ latitude: 37.4862618, longitude: 127.1222903 });
+    }
   }, []);
 
-  return {};
+  useEffect(() => {
+    if (typeof myLocation !== "string") {
+      let currentPosition = [myLocation.latitude, myLocation.longitude];
+
+      mapRef.current = new naver.maps.Map("map", {
+        center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
+        zoomControl: true,
+      });
+    }
+  }, [myLocation]);
+
+  return {
+    myLocation,
+  };
 }
 
 export default useMap;
