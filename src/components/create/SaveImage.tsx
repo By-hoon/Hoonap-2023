51af8f378418fb529ef0d402622f6e1cb36d80e8
Yeunhun Bae <sbaru1997@naver.com>
2023-04-14 22:03:42 +0900

    feat: 미리보기 구현

--- src/components/create/SaveImage.tsx
+++ src/components/create/SaveImage.tsx
@@ -1,13 +1,30 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 
 const SaveImage = () => {
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
+
   const uploadImage = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.files);
+    e.preventDefault();
+    if (e.currentTarget.files !== null) {
+      const fileBlobs = e.currentTarget.files;
+      encodeFileToBase64(fileBlobs);
+    }
+  };
+
+  const encodeFileToBase64 = (fileBlobs: FileList) => {
+    const filesArray = Array.from(fileBlobs).map((file) => URL.createObjectURL(file));
+    setPreviewImages((prevImages) => prevImages.concat(filesArray));
   };
 
   return (
     <div>
       <input type="file" accept="image/*" multiple onChange={uploadImage} />
+      {previewImages.map((imageUrl, index) => (
+        <div key={index}>
+          <Image src={imageUrl} alt="preview-image" width={150} height={150} />
+        </div>
+      ))}
     </div>
   );
 };
