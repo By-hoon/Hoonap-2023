c0e806011f5b9590323bb60d6b1993d7f49a3a83
Yeunhun Bae <sbaru1997@naver.com>
2023-05-02 19:32:23 +0900

    feat: 스토리 리스트의 맵, 다각형 오버레이 구현

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,7 +6,7 @@ interface MapProps {
     latitude: number;
     longitude: number;
   }[];
-  setPaths: Dispatch<
+  setPaths?: Dispatch<
     SetStateAction<
       {
         latitude: number;
@@ -20,18 +20,6 @@ export default function Map({ paths, setPaths }: MapProps) {
   const [myLocation, setMyLocation] = useState<{ latitude: number; longitude: number } | string>("");
 
   const navermaps = useNavermaps();
-
-  const addPaths = (path: { latitude: number; longitude: number }) => {
-    const newPaths = [...paths];
-    newPaths.push(path);
-    setPaths(newPaths);
-  };
-
-  const deletePaths = (index: number) => {
-    const newPaths = [...paths];
-    newPaths.splice(index, 1);
-    setPaths(newPaths);
-  };
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -49,6 +37,73 @@ export default function Map({ paths, setPaths }: MapProps) {
       longitude: 127.1222903,
     });
   }, []);
+  const mapChildRender = () => {
+    switch (setPaths) {
+      case undefined: {
+        const polygons: { latitude: number; longitude: number }[][] = [];
+        let polyPaths: { latitude: number; longitude: number }[] = [];
+        paths.forEach((path) => {
+          if (path.latitude === 0) {
+            polygons.push([...polyPaths]);
+            polyPaths = [];
+            return;
+          }
+          polyPaths.push(path);
+        });
+        return (
+          <>
+            {polygons.map((polygon, index) => (
+              <Polygon
+                key={index}
+                paths={[polygon.map((path) => new navermaps.LatLng(path.latitude, path.longitude))]}
+                fillColor="#ff0000"
+                fillOpacity={0.3}
+                strokeColor="#ff0000"
+                strokeOpacity={0.6}
+                strokeWeight={3}
+              />
+            ))}
+          </>
+        );
+      }
+      default: {
+        const addPaths = (path: { latitude: number; longitude: number }) => {
+          const newPaths = [...paths];
+          newPaths.push(path);
+          setPaths(newPaths);
+        };
+
+        const deletePaths = (index: number) => {
+          const newPaths = [...paths];
+          newPaths.splice(index, 1);
+          setPaths(newPaths);
+        };
+        return (
+          <>
+            <Listener
+              type="click"
+              listener={(e) => addPaths({ latitude: e.coord.lat(), longitude: e.coord.lng() })}
+            />
+            <Polygon
+              paths={[paths.map((path) => new navermaps.LatLng(path.latitude, path.longitude))]}
+              fillColor="#ff0000"
+              fillOpacity={0.3}
+              strokeColor="#ff0000"
+              strokeOpacity={0.6}
+              strokeWeight={3}
+            />
+            {paths.map((path, index) => (
+              <Marker
+                key={index}
+                position={new navermaps.LatLng(path.latitude, path.longitude)}
+                onClick={() => deletePaths(index)}
+              />
+            ))}
+          </>
+        );
+      }
+    }
+  };
 
   return (
     <MapDiv id="map" className="w-[400px] h-[400px]">
@@ -57,25 +112,7 @@ export default function Map({ paths, setPaths }: MapProps) {
           defaultCenter={new navermaps.LatLng(myLocation.latitude, myLocation.longitude)}
           defaultZoom={15}
         >
-          <Listener
-            type="click"
-            listener={(e) => addPaths({ latitude: e.coord.lat(), longitude: e.coord.lng() })}
-          />
-          <Polygon
-            paths={[paths.map((path) => new navermaps.LatLng(path.latitude, path.longitude))]}
-            fillColor="#ff0000"
-            fillOpacity={0.3}
-            strokeColor="#ff0000"
-            strokeOpacity={0.6}
-            strokeWeight={3}
-          />
-          {paths.map((path, index) => (
-            <Marker
-              key={index}
-              position={new navermaps.LatLng(path.latitude, path.longitude)}
-              onClick={() => deletePaths(index)}
-            />
-          ))}
+          {mapChildRender()}
         </NaverMap>
       ) : null}
     </MapDiv>
--- src/pages/story/list.tsx
+++ src/pages/story/list.tsx
@@ -1,16 +1,45 @@
 import getData from "@/firebase/firestore/getData";
 import { DocumentData } from "firebase/firestore";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("@/components/Map"), {
+  ssr: false,
+});
 
-const List = (stories: DocumentData[]) => {
-  console.log(stories);
-  return <div></div>;
+interface ListProps {
+  stories: DocumentData[];
+}
+
+const List = ({ stories }: ListProps) => {
+  const editPaths = () => {
+    const polyPaths: {
+      latitude: number;
+      longitude: number;
+    }[] = [];
+
+    stories.forEach((story) => {
+      story.paths.forEach((path: { latitude: number; longitude: number }) => {
+        polyPaths.push(path);
+      });
+      polyPaths.push({
+        latitude: 0,
+        longitude: 0,
+      });
+    });
+    return polyPaths;
+  };
+  editPaths();
+  return (
+    <div>
+      <Map paths={editPaths()} />
+    </div>
+  );
 };
 
 export default List;
 
 export const getServerSideProps = async () => {
   const result = await getData("stories");
-  const stories: DocumentData[] = [];
+  const stories: DocumentData = [];
 
   if (result)
     result.forEach((e) => {
