444dd15acacf6251a4be7b99a4d3d9025a5f8482
Yeunhun Bae <sbaru1997@naver.com>
2023-05-05 18:22:37 +0900

    feat: firebase 로그인 구현

---
+++ src/firebase/auth/signIn.ts
@@ -0,0 +1,13 @@
+import firebase_app from "../config";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(firebase_app);
+
+export default async function signIn(email: string, password: string) {
+  try {
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
