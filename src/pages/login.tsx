9298ce82963cf35e2b7c360e935a45cc6ff18be9
Yeunhun Bae <sbaru1997@naver.com>
2023-05-05 23:37:25 +0900

    feat: 로그인 페이지 구현

---
+++ src/pages/login.tsx
@@ -0,0 +1,51 @@
+import Title from "@/components/common/Title";
+import signIn from "@/firebase/auth/signIn";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const tryLogin = async () => {
+    const result = await signIn(email, password);
+    if (!result) return;
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <Title title="로그인" />
+      <div>
+        <input
+          type="text"
+          value={email}
+          placeholder="아이디를 입력해 주세요"
+          onChange={changeEmail}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="비밀번호를 입력해 주세요"
+          onChange={changePassword}
+          required
+        />
+      </div>
+      <button onClick={tryLogin}>로그인</button>
+    </div>
+  );
+};
+
+export default Login;
