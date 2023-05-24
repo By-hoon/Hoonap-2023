c36a70d26c2b84d53993c6e40735612e75714d7e
Yeunhun Bae <sbaru1997@naver.com>
2023-05-24 13:25:29 +0900

    feat: 서버 측에서 사용자 id로 paths 정보 호출 구현

--- src/pages/story/list.tsx
+++ src/pages/story/list.tsx
@@ -1,12 +1,18 @@
 import { GetServerSidePropsContext } from "next/types";
 import dynamic from "next/dynamic";
 import checkUser from "@/firebase/auth/checkUser";
+import getDocument from "@/firebase/firestore/getDocument";
+import { DocumentData } from "firebase/firestore";
 const Map = dynamic(() => import("@/components/Map"), {
   ssr: false,
 });
 
-const List = () => {
-  return <div>{/* <Map paths={} /> */}</div>;
+const List = ({ paths }: { paths: { latitude: number; longitude: number }[] }) => {
+  return (
+    <div>
+      <Map paths={paths} />
+    </div>
+  );
 };
 
 export default List;
@@ -17,7 +23,22 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
     return {
       props: {} as never,
     };
+  const usersResult = await getDocument("users", uid);
+  if (!usersResult)
+    return {
+      props: {} as never,
+    };
+  const paths: { latitude: number; longitude: number }[] = [];
+
+  const promises = usersResult.storyIds.map(async (storyId: string) => {
+    const pathsResult = await getDocument("paths", storyId);
+    if (!pathsResult) return;
+    paths.push(pathsResult.paths);
+  });
+
+  await Promise.all(promises);
+
   return {
-    props: { uid },
+    props: { paths },
   };
 };
