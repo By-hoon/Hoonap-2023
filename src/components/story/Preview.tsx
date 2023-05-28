e9daa12d7131fc89669b3f837a6b85f0ed942cc9
By-hoon <sbaru1997@naver.com>
2023-05-28 23:56:21 +0900

    feat: 스토리 미리보기 컴포넌트 스토리 정보 호출

--- src/components/story/Preview.tsx
+++ src/components/story/Preview.tsx
@@ -1,8 +1,19 @@
+import getDocument from "@/firebase/firestore/getDocument";
+import { useEffect } from "react";
+
 interface PreviewProps {
   currentStoryId: string;
 }
 
 const Preview = ({ currentStoryId }: PreviewProps) => {
+  const getStory = async () => {
+    const result = await getDocument("stories", currentStoryId);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getStory();
+  });
   return <div>스토리 미리보기</div>;
 };
 
