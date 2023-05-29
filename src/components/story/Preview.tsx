3718c3981a96d857dd43f5e242d9f43df86b4479
By-hoon <sbaru1997@naver.com>
2023-05-29 17:38:55 +0900

    feat: 스토리 미리보기 컴포넌트 구현

--- src/components/story/Preview.tsx
+++ src/components/story/Preview.tsx
@@ -1,20 +1,47 @@
 import getDocument from "@/firebase/firestore/getDocument";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface PreviewProps {
   currentStoryId: string;
 }
 
 const Preview = ({ currentStoryId }: PreviewProps) => {
+  const [title, setTitle] = useState("");
+  const [story, setStory] = useState("");
+  const [images, setImages] = useState<string[]>([]);
+
   const getStory = async () => {
     const result = await getDocument("stories", currentStoryId);
-    console.log(result);
+    if (!result) return;
+    setImages(result.images);
+    setTitle(result.title);
+    setStory(result.story);
   };
-
   useEffect(() => {
     getStory();
-  });
-  return <div>스토리 미리보기</div>;
+  }, [currentStoryId]);
+  return (
+    <div>
+      <div>
+        <div>{title}</div>
+        <div>{story}</div>
+      </div>
+      <div>
+        {images.map((imageUrl, index) => (
+          <figure key={index}>
+            <Image
+              src={imageUrl}
+              alt="preview-image"
+              width={150}
+              height={150}
+              style={{ width: 150, height: 150 }}
+            />
+          </figure>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Preview;
