cda84f2b5ff8e5ffb3f56bb233c523f68c704a8d
Yeunhun Bae <sbaru1997@naver.com>
2023-06-04 18:58:24 +0900

    feat: 사용자 정보 페이지, 사용자 스토리 정보 호출

--- src/pages/user/detail.tsx
+++ src/pages/user/detail.tsx
@@ -1,7 +1,14 @@
 import getDocument from "@/firebase/firestore/getDocument";
+import { DocumentData } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 
-const UserDetail = () => {
+interface UserDetailProps {
+  paths: [{ latitude: number; longitude: number }[]];
+  images: { urls: string[]; id: string }[];
+}
+
+const UserDetail = ({ paths, images }: UserDetailProps) => {
+  console.log(paths, images);
   return <div>사용자 정보</div>;
 };
 
@@ -9,10 +16,22 @@ export default UserDetail;
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const userId = context.query.userId as string;
-  const result = await getDocument("users", userId);
-  if (!result) return;
+  const userStoryResult = await getDocument("users", userId);
+  if (!userStoryResult) return;
+
+  const paths: [{ latitude: number; longitude: number }[]] = [[]];
+  const images: { urls: string[]; id: string }[] = [];
+
+  const promises = userStoryResult.storyIds.map(async (storyId: string) => {
+    const storyResult = await getDocument("stories", storyId);
+    if (!storyResult) return;
+    paths.push(storyResult.paths);
+    images.push({ urls: storyResult.images, id: storyId });
+  });
+
+  await Promise.all(promises);
 
   return {
-    props: {},
+    props: { paths: paths, images: images },
   };
 };
