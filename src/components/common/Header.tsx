bff4ab9aa1ea25926531de4654c5324772e82468
Yeunhun Bae <sbaru1997@naver.com>
2023-06-20 20:20:55 +0900

    feat: 헤더 컴포넌트 모바일 뷰 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,69 @@
 import doSignOut from "@/firebase/auth/signOut";
+import { Icon } from "@iconify/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   const router = useRouter();
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
+
+  const openSidebar = () => {
+    setShowSidebar(true);
+  };
+  const closeSidebar = () => {
+    setShowSidebar(false);
+  };
+
+  if (isMobile)
+    return (
+      <div>
+        <div>
+          <div onClick={openSidebar}>
+            <Icon icon="material-symbols:menu-rounded" />
+          </div>
+          {showSidebar ? (
+            <div>
+              {/* 그림자 배경용 div */}
+              <div>
+                <div>
+                  <Icon icon="material-symbols:close" onClick={closeSidebar} />
+                </div>
+                <div>
+                  <Link href="/">Hoonap</Link>
+                </div>
+                <div>
+                  <div>프로필</div>
+                  <div
+                    onClick={() => {
+                      doSignOut();
+                      router.replace("/login");
+                    }}
+                  >
+                    로그아웃
+                  </div>
+                </div>
+                <div>
+                  <Link href="/story/list">스토리</Link>
+                </div>
+              </div>
+            </div>
+          ) : null}
+        </div>
+        <div>
+          <Link href="/">Hoonap</Link>
+        </div>
+        <div>
+          <Link href="/create">생성</Link>
+        </div>
+      </div>
+    );
+
   return (
     <div>
       <div>
