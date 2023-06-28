509f283709a06a51801cdb89f4f88acf09c67a6e
Yeunhun Bae <sbaru1997@naver.com>
2023-06-28 20:08:53 +0900

    feat: 헤더에 타이틀 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "react-responsive";
+import Title from "./Title";
+import { title } from "@/shared/constants";
 
 const Header = () => {
   const [mounted, setMounted] = useState(false);
@@ -47,6 +49,22 @@ const Header = () => {
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  const titleRender = () => {
+    switch (router.pathname) {
+      case "/create": {
+        return <Title title={title.create} />;
+      }
+      case "/story/detail": {
+        return <Title title={title.story} />;
+      }
+      case "/story/list": {
+        return <Title title={title.stories} />;
+      }
+      default:
+        return null;
+    }
+  };
 
   if (!mounted) return <></>;
   if (isMobile)
@@ -94,42 +112,45 @@ const Header = () => {
     );
 
   return (
-    <div className="flex justify-between w-100% max-w-[1200px] h-[70px] px-[10px] border-b-2 border-bs">
-      <div className="flex">
-        <div className="pt-[12px]">
-          <Link href="/" className="font-extrabold text-[24px]">
-            Hoonap
-          </Link>
-        </div>
-        <div className="flex items-center w-[120px] font-semibold text-[18px] ml-[20px]">
-          <div>
-            <Link href="/story/list" className="hover:text-bc">
-              스토리
+    <div className="fixed top-0 left-0 w-[100%] h-[70px] px-[10px] border-b-2 border-bs">
+      <div className="flex justify-between max-w-[1200px] h-[100%] mx-auto my-0">
+        <div className="flex">
+          <div className="pt-[12px]">
+            <Link href="/" className="font-extrabold text-[24px]">
+              Hoonap
             </Link>
           </div>
-        </div>
-      </div>
-      <div className="flex items-center">
-        <div ref={profileMenuRef} className="relative flex  font-semibold text-[18px] mr-[30px]">
-          <div className="cursor-pointer" onClick={controllProfileMenu}>
-            프로필
-          </div>
-          {showProfileMenu ? (
-            <div className="absolute top-[30px] left-[-12px] w-[80px] h-[60px] text-center font-normal text-[15px] bg-white rounded-[6px] shadow-basic p-[3px]">
-              <div
-                className="cursor-pointer"
-                onClick={() => {
-                  doSignOut();
-                  router.replace("/login");
-                }}
-              >
-                로그아웃
-              </div>
+          <div className="flex items-center w-[120px] font-semibold text-[18px] ml-[20px]">
+            <div>
+              <Link href="/story/list" className="hover:text-bc">
+                스토리
+              </Link>
             </div>
-          ) : null}
+          </div>
         </div>
-        <div className="flex justify-center items-center w-[65px] h-[40px] font-medium text-[16px] text-white rounded-[5px] bg-bc">
-          <Link href="/create">생성</Link>
+        <div className="flex items-end font-semibold text-[18px] px-[10px] pb-[10px]">{titleRender()}</div>
+        <div className="flex items-center">
+          <div ref={profileMenuRef} className="relative flex  font-semibold text-[18px] mr-[30px]">
+            <div className="cursor-pointer" onClick={controllProfileMenu}>
+              프로필
+            </div>
+            {showProfileMenu ? (
+              <div className="absolute top-[30px] left-[-12px] w-[80px] h-[60px] text-center font-normal text-[15px] bg-white rounded-[6px] shadow-basic p-[3px]">
+                <div
+                  className="cursor-pointer"
+                  onClick={() => {
+                    doSignOut();
+                    router.replace("/login");
+                  }}
+                >
+                  로그아웃
+                </div>
+              </div>
+            ) : null}
+          </div>
+          <div className="flex justify-center items-center w-[65px] h-[40px] font-medium text-[16px] text-white rounded-[5px] bg-bc">
+            <Link href="/create">생성</Link>
+          </div>
         </div>
       </div>
     </div>
