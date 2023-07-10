16df27db6fdbffe85568eeb117b8f0e1c88ad52d
Yeunhun Bae <sbaru1997@naver.com>
2023-07-10 20:38:54 +0900

    feat: 갤러리에서 이미지 클릭 시 확대된 이미지와 미리보기 팝업

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -4,19 +4,22 @@ import Image from "next/image";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Preview from "@/components/story/Preview";
 
 const Gallery = () => {
-  const [images, setImages] = useState<{ url: string; id: string }[]>([]);
+  const [images, setImages] = useState<{ url: string; userId: string; id: string }[]>([]);
+  const [current, setCurrent] = useState<{ url: string; userId: string; id: string }>();
   const router = useRouter();
 
   const getImageData = async () => {
     const result = await getCollection("images");
     if (!result) return;
 
-    const newData: { url: string; id: string }[] = [];
+    const newData: { url: string; userId: string; id: string }[] = [];
     result.docs.forEach((doc) => {
+      const userId = doc.data().userId;
       doc.data().fileUrls.forEach((url: string) => {
-        newData.push({ url, id: doc.id });
+        newData.push({ url, userId, id: doc.id });
       });
     });
     setImages(newData);
@@ -36,17 +39,38 @@ const Gallery = () => {
   return (
     <Layout>
       <div>
-        {images.map((imageObj, index) => (
-          <figure key={index}>
-            <Image
-              src={imageObj.url}
-              alt="preview-image"
-              width={150}
-              height={150}
-              style={{ width: 150, height: 150 }}
-            />
-          </figure>
-        ))}
+        <div>
+          {images.map((imageObj, index) => (
+            <figure key={index}>
+              <Image
+                src={imageObj.url}
+                alt="preview-image"
+                width={150}
+                height={150}
+                style={{ width: 150, height: 150 }}
+                onClick={() => {
+                  setCurrent(imageObj);
+                }}
+              />
+            </figure>
+          ))}
+        </div>
+        {current ? (
+          <div className="border-2">
+            <figure>
+              <Image
+                src={current.url}
+                alt="preview-image"
+                width={450}
+                height={450}
+                style={{ width: 450, height: 450 }}
+              />
+            </figure>
+            <div>
+              <Preview currentStoryId={current.id} userId={current.userId} />
+            </div>
+          </div>
+        ) : null}
       </div>
     </Layout>
   );
