4716ccb135740aa843ba0d1153235af0d9b5030c
Yeunhun Bae <sbaru1997@naver.com>
2023-07-19 17:50:36 +0900

    feat: admin config 수정

--- src/firebase/adminConfig.ts
+++ src/firebase/adminConfig.ts
@@ -1,8 +1,14 @@
 import admin from "firebase-admin";
 
+const firebaseAdminConfig = {
+  privateKey: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, "\n"),
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(firebaseAdminConfig),
+    databaseURL: "https://hoowitter-default-rtdb.asia-southeast1.firebasedatabase.app",
   });
 }
 
