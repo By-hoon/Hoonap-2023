6c2501b8a259c0ece410f531ee52a855833cc191
Yeunhun Bae <sbaru1997@naver.com>
2023-07-21 18:19:23 +0900

    style: 타이틀 컴포넌트 스타일링

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -3,7 +3,7 @@ interface TitleProps {
 }
 
 const Title = ({ title }: TitleProps) => {
-  return <div>{title}</div>;
+  return <div className="text-[24px] text-bc px-[10px] py-[7px] mb-[40px] border-b border-bs">{title}</div>;
 };
 
 export default Title;
