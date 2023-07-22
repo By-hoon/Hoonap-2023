58885e99567bb1ecd45eaf27fc1aeec16f31fcac
Yeunhun Bae <sbaru1997@naver.com>
2023-07-22 19:53:00 +0900

    style: 비로그인 메인 페이지 스타일링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,13 +36,21 @@ export default function Home({ loggedIn, uid }: { loggedIn: boolean; uid: string
 
   if (!isLoggedIn)
     return (
-      <div>
-        <div>안내문구</div>
-        <Link href="/login">
-          <button>로그인</button>
-        </Link>
-        <div>
-          <button onClick={tryLoginTestAccount}>체험하기</button>
+      <div className="max-w-[768px] min-w-[320px] mx-auto p-[5px]">
+        <div className="text-center text-[22px] md:text-[24px] font-semibold">Hoonap 방문을 환영합니다 !</div>
+        <div className="flex justify-between w-[190px] mx-auto mt-[20px]">
+          <Link href="/signup">
+            <button className="submit-button px-[15px]">회원가입</button>
+          </Link>
+          <Link href="/login">
+            <button className="submit-button">로그인</button>
+          </Link>
+        </div>
+        <div className="text-center mt-[40px]">미리 서비스를 사용해 보고 싶다면?</div>
+        <div className="mt-[10px] text-center">
+          <button className="submit-button bg-gray-400 hover:bg-gray-500" onClick={tryLoginTestAccount}>
+            체험하기
+          </button>
         </div>
       </div>
     );
