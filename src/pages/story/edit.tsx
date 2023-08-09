9a9c550f98540f006114da45a7dc3545b311a30d
Yeunhun Bae <sbaru1997@naver.com>
2023-08-09 23:31:02 +0900

    feat: 스토리 수정 페이지 구현

--- src/pages/story/edit.tsx
+++ src/pages/story/edit.tsx
@@ -1,11 +1,88 @@
+import Layout from "@/components/common/Layout";
+import SaveImage from "@/components/create/SaveImage";
+import SavePath from "@/components/create/SavePath";
 import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
 
 const StoryEdit = () => {
   const router = useRouter();
 
-  const { title, story, images, paths, userId, storyId } = router.query;
+  const {
+    title: queryTitle,
+    story: queryStory,
+    images: queryImageUrls,
+    paths: queryPaths,
+    userId,
+    storyId,
+  } = router.query;
+  const [paths, setPaths] = useState<{ latitude: number; longitude: number }[]>(
+    JSON.parse(queryPaths as string)
+  );
+  const [images, setImage] = useState<FileList>();
+  const [previewImages, setPreviewImages] = useState<string[]>(queryImageUrls as string[]);
+  const [title, setTitle] = useState(queryTitle);
+  const [story, setStory] = useState(queryStory);
 
-  return <div>edit story</div>;
+  const changeTitle = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }, []);
+
+  const changeStory = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setStory(e.target.value);
+  }, []);
+
+  const editStory = async () => {};
+
+  return (
+    <Layout>
+      <div className="md:w-[670px] mx-auto p-[10px]">
+        <div className="main-relative border-b-2">
+          <SavePath paths={paths} setPaths={setPaths} />
+        </div>
+        <div className="main-relative border-b-2">
+          <SaveImage
+            images={images}
+            setImage={setImage}
+            previewImages={typeof previewImages === "string" ? [previewImages] : previewImages}
+            setPreviewImages={setPreviewImages}
+          />
+        </div>
+        <div className="main-relative border-b-2">
+          <div className="main-absolute">
+            <div className="min-w-[420px] max-w-[550px] h-[40%] mx-auto my-0 p-[10px] flex flex-wrap content-between">
+              <div className="w-[100%] border-2 border-bc p-[10px] rounded-[15px]">
+                <input
+                  className="w-[100%] focus:outline-none"
+                  type="text"
+                  value={title}
+                  placeholder="게시물에게 제목을 지어주세요."
+                  onChange={changeTitle}
+                  required
+                />
+              </div>
+              <div className="w-[100%] h-[70%] border-2 border-bc p-[10px] rounded-[15px]">
+                <textarea
+                  className="w-[100%] !h-[100%] focus:outline-none resize-none"
+                  value={story}
+                  placeholder="사진들이 담고 있는 이야기를 들려주세요."
+                  onChange={changeStory}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[100%] text-center mt-[45px]">
+          <button
+            className="text-[19px] font-semibold bg-bc text-white px-[38px] py-[15px] rounded-[12px]"
+            onClick={editStory}
+          >
+            스토리 수정
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default StoryEdit;
