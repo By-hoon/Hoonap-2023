41d4776e32cdadf97a392618749cf347d1cf4b3a
Yeunhun Bae <sbaru1997@naver.com>
2023-09-03 23:26:18 +0900

    feat: 로그인, 회원가입, 스토리 수정 페이지 url 직접 접근 추가 처리

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/signup",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/story/edit",
+        destination: "/story/list",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
