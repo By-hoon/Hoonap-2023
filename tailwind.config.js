ac3b3f77409fc899a89bb7dcd1a2db1069eb4ae8
Yeunhun Bae <sbaru1997@naver.com>
2023-09-27 18:43:42 +0900

    style: 모바일 헤더 스타일링

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
         basic: "0px 3px 10px 0px rgba(0, 0, 0, 0.3)",
         underblue: "0px 25px 20px -15px #00a8ff, 0px 0px 10px 0px rgb(0 0 0 / 0.1)",
       },
+      keyframes: {
+        appearSidebar: {
+          "0%": {
+            transform: "translate(-20px, 0px)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px)",
+          },
+        },
+      },
+      animation: {
+        appearSidebar: "appearSidebar 0.3s forwards",
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
