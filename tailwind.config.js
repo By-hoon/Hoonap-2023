240645fe5e350b71369deb3b31ced0cfa874f11c
By-hoon <sbaru1997@naver.com>
2023-09-29 23:51:08 +0900

    style: 모바일 Responsive Screen 등록

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      mobile: { min: "300px", max: "767px" },
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         bc: "#00a8ff",
