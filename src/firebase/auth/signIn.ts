d504746a0a832a32f131dc7187605539020fb4ba
Yeunhun Bae <sbaru1997@naver.com>
2023-10-04 18:36:24 +0900

    feat: 로그인 오류 처리

--- src/firebase/auth/signIn.ts
+++ src/firebase/auth/signIn.ts
@@ -1,3 +1,4 @@
+import { FirebaseError } from "firebase/app";
 import firebase_app from "../config";
 import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 
@@ -7,7 +8,20 @@ export default async function signIn(email: string, password: string) {
   try {
     return await signInWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    console.log(error);
+    if (error instanceof FirebaseError) {
+      if (error.code == "auth/invalid-email") {
+        alert("이메일 형식을 확인해 주세요.");
+      }
+      if (error.code == "auth/user-not-found") {
+        alert("아이디를 확인해 주세요.");
+      }
+      if (error.code == "auth/wrong-password") {
+        alert("비밀번호를 확인해 주세요");
+      }
+      if (error.code == "auth/too-many-requests") {
+        alert("잠시 후 다시 시도해 주세요");
+      }
+    }
     return false;
   }
 }
