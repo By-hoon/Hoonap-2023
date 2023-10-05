62b19702a7758b62801471a85e6f0518d38725ba
Yeunhun Bae <sbaru1997@naver.com>
2023-10-05 20:07:48 +0900

    feat: 회원가입 오류 처리

--- src/firebase/auth/signUp.ts
+++ src/firebase/auth/signUp.ts
@@ -1,3 +1,4 @@
+import { FirebaseError } from "firebase/app";
 import firebase_app from "../config";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
@@ -7,7 +8,20 @@ export default async function signUp(email: string, password: string) {
   try {
     return await createUserWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    console.log(error);
+    if (error instanceof FirebaseError) {
+      if (error.code == "auth/email-already-in-use") {
+        alert("이미 있는 아이디입니다.");
+      }
+      if (error.code == "auth/invalid-email") {
+        alert("이메일 형식을 확인해 주세요.");
+      }
+      if (error.code == "auth/weak-password") {
+        alert("비밀번호를 다시 확인해 주세요.");
+      }
+      if (error.code == "auth/too-many-requests") {
+        alert("잠시 후 다시 시도해 주세요");
+      }
+    }
     return false;
   }
 }
