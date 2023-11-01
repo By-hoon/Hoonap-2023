889ad6b8e33cdc7df9c08ff933fe761a949d5489
Yeunhun Bae <sbaru1997@naver.com>
2023-11-01 20:45:09 +0900

    style: 스토리 미리보기 컴포넌트 스타일링 수정

--- src/components/story/Preview.tsx
+++ src/components/story/Preview.tsx
@@ -35,92 +35,93 @@ const Preview = ({ title, story, images, paths, storyId, userId, deleteStory }: 
   };
 
   return (
-    <div className="relative md:grid md:grid-cols-[1fr_300px] min-w-[300px] max-w-[964px] md:mt-[95px] md:mx-[30px] lg:mx-auto border rounded-[5px] overflow-hidden">
-      <div className="main-relative">
-        {images.length !== 0 ? (
-          <BasicImage style={"w-full h-full relative"} url={images[currentIndex]} alt={"detail-image"} />
-        ) : null}
-        {images.length > 1 ? (
-          <div>
-            <div className="flex justify-between w-full absolute top-[50%] left-[0] translate-y-[-50%] text-[28px] text-white opacity-60 px-[10px]">
-              <figcaption className="cursor-pointer" onClick={preImage}>
-                <Icon icon="icon-park-solid:left-c" />
-              </figcaption>
-              <figcaption className="cursor-pointer" onClick={nextImage}>
-                <Icon icon="icon-park-solid:right-c" />
-              </figcaption>
-            </div>
-            <div className="absolute bottom-[10px] left-0 w-full z-20 flex justify-center">
-              {images.map((image, index) => (
-                <div
-                  key={image}
-                  className={`w-[8px] h-[8px] bg-white rounded-[50%] mx-[2px]
-                  ${currentIndex === index ? "opacity-100" : "opacity-40"}`}
-                />
-              ))}
-            </div>
-          </div>
-        ) : null}
-      </div>
-      <div>
-        <div className="flex justify-between h-[60px] border-b-2 mb-[10px] px-[15px]">
-          <div className="h-full flex items-center text-[18px]">
-            <Link
-              href={{
-                pathname: "/user/detail",
-                query: { userId: userId },
-              }}
-              as="/user/detail"
-            >
-              {nickname}
-            </Link>
-          </div>
-          <div className="flex items-center" ref={moreMenuRef}>
-            <Icon icon="ri:more-fill" className=" cursor-pointer text-[32px]" onClick={onClickMoreMenu} />
-            {showMoreMenu ? (
-              <div>
-                <div className="background-shadow" onClick={onClickMoreMenu} />
-                <div className="absolute bottom-0 right-0 mobile:top-0 w-[250px] md:w-[300px] h-[80%] mobile:max-h-[380px] text-white font-semibold text-[18px] bg-zinc-800 rounded-[6px] p-[20px] z-20">
-                  {userId === user?.uid ? (
-                    <div>
-                      <Link
-                        className="cursor-pointer flex items-center text-white mb-[20px]"
-                        href={{
-                          pathname: "/story/edit",
-                          query: {
-                            title,
-                            story,
-                            images,
-                            paths: JSON.stringify(paths),
-                            userId,
-                            storyId,
-                            restExpStory: JSON.stringify({}),
-                          },
-                        }}
-                        as="/story/edit"
-                      >
-                        <Icon className="text-[28px] mr-[8px]" icon="fluent:edit-20-regular" />
-                        수정
-                      </Link>
-                      <div
-                        className="cursor-pointer flex items-center text-red-600 mb-[20px]"
-                        onClick={() => deleteStory(storyId, userId)}
-                      >
-                        <Icon className="text-[28px] mr-[8px]" icon="mingcute:delete-line" />
-                        삭제
-                      </div>
+    <div className="w-[468px] mobile:w-[300px] border-b-2 mx-auto pb-[15px]">
+      <div className="flex justify-between h-[50px]">
+        <div className="h-full flex items-center text-[18px]">
+          <Link
+            href={{
+              pathname: "/user/detail",
+              query: { userId: userId },
+            }}
+            as="/user/detail"
+          >
+            {nickname}
+          </Link>
+        </div>
+        <div className="flex items-center" ref={moreMenuRef}>
+          <Icon icon="ri:more-fill" className=" cursor-pointer text-[32px]" onClick={onClickMoreMenu} />
+          {showMoreMenu ? (
+            <div>
+              <div className="background-shadow !fixed" onClick={onClickMoreMenu} />
+              <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] mobile:w-[250px] font-semibold text-[18px] bg-white rounded-[6px] z-30">
+                {userId === user?.uid ? (
+                  <div>
+                    <Link
+                      className="cursor-pointer flex justify-center items-center m-[5px] p-[5px]"
+                      href={{
+                        pathname: "/story/edit",
+                        query: {
+                          title,
+                          story,
+                          images,
+                          paths: JSON.stringify(paths),
+                          userId,
+                          storyId,
+                          restExpStory: JSON.stringify({}),
+                        },
+                      }}
+                      as="/story/edit"
+                    >
+                      수정
+                    </Link>
+                    <div
+                      className="cursor-pointer flex justify-center items-center text-red-600 m-[5px] p-[5px]"
+                      onClick={() => deleteStory(storyId, userId)}
+                    >
+                      삭제
                     </div>
-                  ) : null}
-                </div>
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
+            </div>
+          ) : null}
         </div>
-        <div className="px-[15px]">
-          <div className="h-[40px] text-[22px]">{title}</div>
-          <div className="text-[18px] mt-[5px]">{story}</div>
+      </div>
+      {images.length !== 0 ? (
+        <BasicImage
+          style={"relative w-full h-[468px] mobile:h-[300px] bg-black rounded-[5px]"}
+          url={images[currentIndex]}
+          alt={"detail-image"}
+        >
+          {images.length > 1 ? (
+            <div>
+              <div className="flex justify-between w-full absolute top-[50%] left-[0] translate-y-[-50%] text-[28px] text-white opacity-60 px-[10px]">
+                <figcaption className="cursor-pointer" onClick={preImage}>
+                  <Icon icon="icon-park-solid:left-c" />
+                </figcaption>
+                <figcaption className="cursor-pointer" onClick={nextImage}>
+                  <Icon icon="icon-park-solid:right-c" />
+                </figcaption>
+              </div>
+              <div className="absolute bottom-[10px] left-0 w-full z-20 flex justify-center">
+                {images.map((image, index) => (
+                  <div
+                    key={image}
+                    className={`w-[8px] h-[8px] bg-white rounded-[50%] mx-[2px]
+                  ${currentIndex === index ? "opacity-100" : "opacity-40"}`}
+                  />
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </BasicImage>
+      ) : null}
+      <div>
+        <div className="flex mt-[5px]">
+          <div className="text-[18px] font-semibold mr-[10px]">{title}</div>
+          <div className="text-[16px] mt-[5px]">{story}</div>
         </div>
-        <div className="text-[18px] text-bc font-semibold">
+        <div className="text-[18px] text-bc text-center">
           <Link
             href={{
               pathname: "/story/detail",
