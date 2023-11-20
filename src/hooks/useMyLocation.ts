612716bdcf7d9b5a1b7b68a17aa1f8ac18dbe7b4
Yeunhun Bae <sbaru1997@naver.com>
2023-11-20 16:59:40 +0900

    fix: 현재 위치를 가져올 수 없는 경우, 기본 위치가 설정되지 않는 오류 수정

--- src/hooks/useMyLocation.ts
+++ src/hooks/useMyLocation.ts
@@ -4,20 +4,24 @@ const useMyLocation = () => {
   const [myLocation, setMyLocation] = useState<{ latitude: number; longitude: number } | string>("");
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setMyLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
+    const success = (position: GeolocationPosition) => {
+      setMyLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
       });
-      return;
+    };
+
+    const error = () => {
+      window.alert("현재 위치를 알 수 없습니다. 기본 위치로 지정합니다.");
+      setMyLocation({
+        latitude: 37.4862618,
+        longitude: 127.1222903,
+      });
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => success(position), error);
     }
-    window.alert("현재 위치를 알 수 없습니다. 기본 위치로 지정합니다.");
-    setMyLocation({
-      latitude: 37.4862618,
-      longitude: 127.1222903,
-    });
   }, []);
 
   return { myLocation };
