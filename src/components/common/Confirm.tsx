95704070d9faae5b727ddf569e73d93a81b1aac1
Yeunhun Bae <sbaru1997@naver.com>
2024-02-17 19:40:39 +0900

    feat: confirm의 클릭 이벤트가 의도에 맞게 이루어지도록 e.stopPropagation 추가

--- src/components/common/Confirm.tsx
+++ src/components/common/Confirm.tsx
@@ -21,16 +21,34 @@ const Confirm = ({ title, content, onClickOK, onClickCancel }: ConfirmProps) => 
   }, [onClickCancel]);
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[300px] bg-white shadow-basic rounded-[10px] overflow-hidden z-[200]">
+    <div
+      className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[300px] bg-white shadow-basic rounded-[10px] overflow-hidden z-[200]"
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
       <div className="text-center break-all p-[25px]">
         <div className="text-[17px] font-semibold">{title}</div>
         <div className="text-[14px] text-neutral-400 mt-[5px]">{content}</div>
       </div>
       <div className="flex text-center text-[16px]">
-        <button className="cursor-pointer w-[50%] bg-zinc-200 py-[7px]" onClick={onClickCancel}>
+        <button
+          className="cursor-pointer w-[50%] bg-zinc-200 py-[7px]"
+          onClick={(e) => {
+            e.stopPropagation();
+            onClickCancel();
+          }}
+        >
           취소
         </button>
-        <button className="cursor-pointer w-[50%] text-white bg-bc py-[7px]" autoFocus onClick={onClickOK}>
+        <button
+          className="cursor-pointer w-[50%] text-white bg-bc py-[7px]"
+          autoFocus
+          onClick={(e) => {
+            e.stopPropagation();
+            onClickOK();
+          }}
+        >
           확인
         </button>
       </div>
