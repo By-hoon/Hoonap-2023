8b7fedf62c8251a0f48c73c381386b2872f185e8
Yeunhun Bae <sbaru1997@naver.com>
2024-03-12 23:09:05 +0900

    feat: 내 정보 페이지에 이미지(카드) 너비 및 행당 개수 계산 함수 적용

--- src/pages/user/story.tsx
+++ src/pages/user/story.tsx
@@ -2,17 +2,21 @@ import Layout from "@/components/common/Layout";
 import DetailView from "@/components/story/DetailView";
 import { StoryProps } from "../story/detail";
 import Router, { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import BasicImage from "@/components/common/BasicImage";
 import withHead from "@/components/hoc/withHead";
 import { ALERT_TITLE, ALERT_CONTENT, HEAD_TITLE, HEAD_DESCRIPTION } from "@/shared/constants";
 import useRegular from "@/hooks/useRegular";
 import { useAuth } from "@/context/authProvider";
 import { PopUpContext } from "@/context/popUpProvider";
+import { cardSizeCalculator } from "@/utils/util";
 
 const Story = () => {
   const [current, setCurrent] = useState<StoryProps>();
   const [rest, setRest] = useState<StoryProps[]>([]);
+  const [cardSize, setCardSize] = useState(0);
+
+  const sizeRef = useRef<HTMLDivElement>(null);
 
   const router = useRouter();
   const { user } = useAuth();
@@ -21,6 +25,7 @@ const Story = () => {
   const { alert } = useContext(PopUpContext);
 
   const { storyId, stories } = router.query;
+
   useEffect(() => {
     if (!storyId) {
       alert(ALERT_TITLE.ACCESS, ALERT_CONTENT.NO_USER);
@@ -47,6 +52,24 @@ const Story = () => {
     setRest(restStories);
   }, [alert, stories, storyId]);
 
+  useEffect(() => {
+    if (!current) return;
+
+    const handleResize = () => {
+      if (!sizeRef.current) return;
+      const curWidth = sizeRef.current?.offsetWidth - 1;
+
+      const curSize = cardSizeCalculator(curWidth);
+
+      setCardSize(curSize);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [current]);
+
   if (!current) {
     return (
       <Layout>
@@ -54,16 +77,21 @@ const Story = () => {
       </Layout>
     );
   }
+
   return (
     <Layout>
       <div className="p-[10px]">
         <DetailView story={current} regular={regular} setRegular={setRegular} />
       </div>
-      <div className="flex flex-wrap justify-between mt-[20px]">
+      <div ref={sizeRef} className="flex flex-wrap mt-[20px]">
         {rest.map((restStory) => (
           <div
             key={restStory.storyId}
-            className="cursor-pointer"
+            className="cursor-pointer mx-[5px] mb-[10px]"
+            style={{
+              width: `${cardSize}px`,
+              height: `${cardSize}px`,
+            }}
             onClick={() => {
               router.push(
                 {
@@ -75,7 +103,7 @@ const Story = () => {
             }}
           >
             <BasicImage
-              style={"relative md:w-[300px] md:h-[300px] w-[150px] h-[150px] mx-[10px] rounded-[8px]"}
+              style={"relative w-full h-full rounded-[10px] bg-black overflow-hidden"}
               url={restStory.images[0]}
               alt={"rest-story-image"}
             />
