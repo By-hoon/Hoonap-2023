17e95d519215d8836d110f1f0b3b54f0d82cbe36
Yeunhun Bae <sbaru1997@naver.com>
2024-03-16 23:26:12 +0900

    refactor: 사용자 상세정보 페이지 맵 옵션 분리

--- src/components/MapOption.tsx
+++ src/components/MapOption.tsx
@@ -34,7 +34,7 @@ export default function MapOption({ paths, clickMap, addPaths, deletePaths }: Ma
               fillOpacity={0.3}
               strokeColor="#ff0000"
               strokeOpacity={0.6}
-              strokeWeight={3}
+              strokeWeight={2}
             />
             {paths[0].pathArray.map((path, index) => (
               <Marker
@@ -45,7 +45,7 @@ export default function MapOption({ paths, clickMap, addPaths, deletePaths }: Ma
             ))}
           </>
         );
-      default:
+      case "/user/detail":
         return (
           <>
             <Listener type="click" listener={() => (clickMap ? clickMap() : undefined)} />
@@ -57,9 +57,26 @@ export default function MapOption({ paths, clickMap, addPaths, deletePaths }: Ma
                 fillOpacity={0.3}
                 strokeColor="#ff0000"
                 strokeOpacity={0.6}
-                strokeWeight={3}
+                strokeWeight={2}
                 clickable
                 onClick={() => (clickMap ? clickMap(index) : undefined)}
+              />
+            ))}
+          </>
+        );
+
+      default:
+        return (
+          <>
+            {paths.map((path, index) => (
+              <Polygon
+                key={index}
+                paths={[convertToLatLng(navermaps, path.pathArray)]}
+                fillColor="#ff0000"
+                fillOpacity={0.3}
+                strokeColor="#ff0000"
+                strokeOpacity={0.6}
+                strokeWeight={2}
               />
             ))}
           </>
