e8b78d98471d7e889fb36c9e95a07cbbcf26e315
Yeunhun Bae <sbaru1997@naver.com>
2024-05-23 23:03:03 +0900

    feat: 활동 로그 페이지 좋아요 정보 호출 구현

--- src/pages/user/log.tsx
+++ src/pages/user/log.tsx
@@ -1,6 +1,26 @@
 import Layout from "@/components/common/Layout";
+import getDocument from "@/firebase/firestore/getDocument";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Log = () => {
+  const [likes, setLikes] = useState<{ imageId: string; imageUrl: string }[]>([]);
+
+  const router = useRouter();
+
+  const { userId } = router.query;
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const getUserLikes = async () => {
+      const likesResult = await getDocument("likes", userId as string);
+      if (!likesResult || likesResult.empty) return;
+    };
+
+    getUserLikes();
+  }, [userId]);
+
   return <Layout>Log</Layout>;
 };
 
